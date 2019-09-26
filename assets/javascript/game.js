a49abaddddf853289b241f7a033f56998e92ea27
Paul Baker <paulpbkr22@gmail.com>
2019-09-26 18:50:49 -0400

    final update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,12 @@
-// The specific letters that the user typed.
 var userChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-// Setting for zero
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
+var guessesLeft  = 9;
 var letterUser = [];
 var eachofLetters = null;
 
 
-// Sets the computerGuess variable equal to a random choice from the computerChoice array.
 var computerGuess = userChoices[Math.floor(Math.random() * userChoices.length)];
 
 function countGuessesLeft() {
@@ -25,10 +22,9 @@ countGuessesLeft();
 var restart = function() {
 	guessesLeft = 9;
 	letterUser = [];
-	var computerGuess = userkbChoices[Math.floor(Math.random() * userasdfqweChoices.length)];
+	var computerGuess = userChoices[Math.floor(Math.random() * userChoices.length)];
 }
 
-// When the user presses a key, it will run the following function..
 document.onkeyup = function(event) {
 	guessesLeft--;
 
